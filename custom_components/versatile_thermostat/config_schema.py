321d2ee483b1a6bfec64be540203ddf1dc989213
Vincent Bakker <mail@bakkerv.nl>
2024-11-04 16:08:45 +0100

    Add minimum value to schema hysteris auto/start/stop

--- custom_components/versatile_thermostat/config_schema.py
+++ custom_components/versatile_thermostat/config_schema.py
@@ -194,7 +194,7 @@ STEP_AUTO_START_STOP = vol.Schema(  # pylint: disable=invalid-name
                 mode="dropdown",
             )
         ),
-        vol.Optional(CONF_AUTO_START_STOP_TEMP_HYSTERERIS, default=CONF_AUTO_START_STOP_TEMP_HYSTERERIS): vol.Coerce(float),
+        vol.Optional(CONF_AUTO_START_STOP_TEMP_HYSTERERIS, default=CONF_AUTO_START_STOP_TEMP_HYSTERERIS): vol.All(vol.Coerce(float), vol.Range(min=0.1)),
     }
 )
 
